d54c4bbbea6cc2e101847462f1b433329565cc2c
Haridha K <kharidhamecse@gmail.com>
2026-08-09 15:33:47 +0530

    Add files via upload

---
+++ assignmentDay2.js
@@ -0,0 +1,16 @@
+let userName = "Haridha";
+console.log("User Name is " + userName);
+
+let orgName = "TestLeaf";
+console.log("Organization Name is " + orgName);
+
+let contactNumber = 1234567890;
+console.log("Contact Number is " + contactNumber);
+
+let knowsAutomation = true;
+console.log("Knows Automation is " + knowsAutomation);
+
+let usesPlaywright;
+console.log("Uses Playwright is " + typeof usesPlaywright);
+
+
